286eb52f5fd5bc47dbaf988d854e005c72e89c44
Max Renner <rennerom@gmail.com>
2021-10-05 00:23:32 -0500

    moved functions, added attributions, some cleanup

--- GitTemplates.psd1
+++ GitTemplates.psd1
@@ -69,10 +69,12 @@
     # NestedModules = @()
     
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = '*'
+    FunctionsToExport = @(
+        'Edit-Gig',
+        'Write-Gig')
     
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-    CmdletsToExport = '*'
+    CmdletsToExport = @()
     
     # Variables to export from this module
     VariablesToExport = '*'
---
+++ functions/Edit-Gig.ps1
@@ -0,0 +1,68 @@
+Function Edit-Gig{
+    <# 
+        .SYNOPSIS
+        Edit an existing .gitignore file by appending text to it.
+        .DESCRIPTION
+        Add files or directories to existing .gitignore file. 
+        Supply your ignore string (or multiple separated with a comma)
+        and a comment (optional) to append to your .gitignore.
+        .PARAMETER Pattern
+        Parameter (p) is mandatory. By specifying the pattern 
+        you are telling Edit-Gig what patterns for files/directories 
+        to append to your .gitignore.
+        .PARAMETER Comment
+        Parameter Comment (c) is optional. You can suppply a comment to help
+        organize/document your .gitignore file. The comment line will appear 
+        before your appended .gitingore patterns. The default is 
+        "Appended on <datestamp>"
+        .EXAMPLE
+        Edit-Gig -p ".txt" -c "Ignore all .txt files"
+
+        Appends the following text to your .gitigore file.
+        -------------------------------------------------
+        |# Ignore all .txt files                        |
+        |.txt                                           |
+
+        .EXAMPLE
+        Edit-Gig -p ".txt, .csv"
+
+        Appends the following text to your .gitigore file.
+        -------------------------------------------------
+        |# Appended on <datestamp>                      |
+        |.txt                                           |
+        |.csv                                           |
+
+        .LINK
+        https://www.github.com/rennerom/GitTemplates
+    #>
+    
+    # ----------------------params
+    param(
+        # pattern for file/dir (can be a list)
+        [Alias('p')]
+        [string]$Pattern,
+
+        # Comment line
+        [Parameter(Mandatory=$false)]
+        [Alias('c')]
+        [string]$Comment = "Appended on $(Get-Date -Format M/d/yy)"
+        )
+
+    # Check if .gitignore is there
+
+    if(-not(Test-Path -Path .gitignore -PathType Leaf)) {  
+
+        Write-Host "`nNo .gitignore file found in this directory`n"
+        
+    } else {
+
+        # Set Comment String and list string
+        $CommentString = "# $Comment"
+        $PatternString = $Pattern.Split(",").Trim()
+
+        $FullOutputText = "$CommentString`n$PatternString"
+
+        write-host("Appending to .gitignore`n--------------------`n$FullOutputText")
+        Add-Content -Path .gitignore -Value "`n$FullOutputText"
+    }
+}
